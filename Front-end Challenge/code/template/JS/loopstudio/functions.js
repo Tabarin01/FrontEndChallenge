929a227a47b597e630468d5cde4941e8ccaddadc
Tabarin01 <119614157+Tabarin01@users.noreply.github.com>
2023-05-18 11:34:27 +0200

    #

---
+++ Front-end Challenge/code/template/JS/loopstudio/functions.js
@@ -0,0 +1,26 @@
+const navItems = document.querySelectorAll(".nav-items");
+const lNav = document.getElementById("l-Nav");
+const openBtn = document.getElementById("open-btn");
+const menu = document.getElementById("menu");
+
+function animateMenu() {
+  // Interchange open and close icons
+  if (menu.className === "fas fa-bars") {
+    menu.setAttribute("class", "fas fa-times");
+  } else {
+    menu.setAttribute("class", "fas fa-bars");
+  }
+
+  // Toggle background
+  lNav.classList.toggle("change");
+
+  // Body toggle
+  document.body.classList.toggle("fixed");
+
+  // Toggle navigation items
+  navItems.forEach((item) => {
+    item.classList.toggle("show");
+  });
+}
+
+openBtn.addEventListener("click", animateMenu);
