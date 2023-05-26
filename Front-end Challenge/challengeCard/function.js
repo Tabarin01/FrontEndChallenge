70668ea4bb582cd225b579800561f43b89e2a5ae
Tabarin01 <119614157+Tabarin01@users.noreply.github.com>
2023-05-26 18:49:45 +0200

    commentedjs

--- Front-end Challenge/challengeCard/function.js
+++ Front-end Challenge/challengeCard/function.js
@@ -1,4 +1,5 @@
-const cardNumber = document.getElementById("number");
+// Dichiarazione e associazioni variabili dal documento html
+const cardNumber = document.getElementById("number"); //getElementById è un metodo specifico per selezionare un elemento HTML tramite il suo ID univoco
 const numberInp = document.getElementById("card_number");
 
 const cardName = document.getElementById("name");
@@ -15,12 +16,16 @@ const cvcInp = document.getElementById("card_cvc");
 
 const submitBtn = document.getElementById("submit_btn");
 
-const compeleted = document.querySelector(".thank");
+const completed = document.querySelector(".thank"); //querySelector è un metodo più versatile e potente che consente di selezionare elementi HTML utilizzando selettori CSS
 const form = document.querySelector("form");
 const continueBtn = document.getElementById("continue_btn");
 
 function setCardNumber(e) {
-  cardNumber.innerText = format(e.target.value);
+  //prende un evento 'e' come argomento. l'evento e' il keyup richiamato in fondo al codice
+  cardNumber.innerText = format(e.target.value); //quindi questo evento e' il target del value ottenuto dal innerText
+  //innerHTML: Restituisce o imposta il contenuto HTML di un elemento
+  //innerText: Restituisce o imposta il testo visibile di un elemento.
+  //appendChild: Aggiunge un nodo figlio all'interno di un altro nodo
 }
 function setCardName(e) {
   cardName.innerText = e.target.value;
@@ -36,13 +41,15 @@ function setCardCvc(e) {
 }
 function format(s) {
   return s.toString().replace(/\d{4}(?=.)/g, "$&");
+  //L'espressione regolare /d{4}(?=.)/g viene utilizzata per trovare tutti i gruppi di 4 cifre che sono seguiti da un carattere diverso da un a capo nella stringa s
+  //La stringa di sostituzione "$&" rappresenta il testo corrispondente all'espressione regolare trovata. In questo caso, viene utilizzato per mantenere inalterato il gruppo di 4 cifre corrispondente.
 }
 
 function handleSubmit(e) {
   e.preventDefault();
   if (!nameInp.value) {
-    nameInp.classList.add("error");
-    nameInp.parentElement.classList.add("error_message");
+    nameInp.classList.add("error"); //aggiunge la classe CSS error
+    nameInp.parentElement.classList.add("error_message"); //toglie la classe CSS error_message dopo la correzione dell'errore
   } else {
     nameInp.classList.remove("error");
     nameInp.parentElement.classList.remove("error_message");
@@ -83,7 +90,7 @@ function handleSubmit(e) {
     cvcInp.value &&
     numberInp.value.length == 16
   ) {
-    compeleted.classList.remove("hidden");
+    completed.classList.remove("hidden");
     form.classList.add("hidden");
   }
 }
